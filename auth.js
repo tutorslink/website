dc6962004fdbaf67526e26166e3a727250d6e2b2
Taf <16181tee@gmail.com>
2026-02-09 15:28:30 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged
-} from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+} from 'firebase/auth';
 
 // ============================================
 // Sign Up Function
